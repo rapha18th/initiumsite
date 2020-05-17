00c166be0593424ceef706d5a5ebc1e96f31820d
rapha18th <rairorr@gmail.com>
2020-05-17 18:16:24 +0200

    i

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import "./index.css"
+import React from "react"
+
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+ 
+  
    
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+      <iframe src="https://free.timeanddate.com/countdown/i7aec19s/n96/cf100/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cacfff/cpcfff/pc4682b4/tc66c/fs100/szw320/szh135/tac000/tpc000/macfff/mpcfff/iso2020-05-23T00:00:00" allowTransparency="true" frameborder="0"  width="100%" 
+    height="100%"></iframe>
 
-  </Layout>
+  
+   
+ 
 )
 
 export default IndexPage
